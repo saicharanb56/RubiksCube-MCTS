softmax bug resolved

train2.py
@@ -19,7 +19,7 @@ parser.add_argument('--nsequences', default=500, type=int, help='Number of seque
 parser.add_argument('--gpu', default='0', type=str)
 parser.add_argument('--wd', default=0, type=int, help="Weight decay")
 parser.add_argument('--momentum', default=0, type=int, help="Momentum")
-parser.add_argument('--tau', default=1, type=float, help="Interpolation parameter in soft update")
+parser.add_argument('--tau', default=0.01, type=float, help="Interpolation parameter in soft update")
 parser.add_argument('--device', default="cuda", type=str)
 
 parser.add_argument('--resume_path', default=None, type=str, help="Path of model dict to resume from")
@@ -155,6 +155,8 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
         # forward pass
         with torch.no_grad():
             p_out, v_out = model_target(next_states)
+
+            print("Probs mean: ", p_out.mean(dim=0))
 
             # set labels to be maximal value from each children state
             v_label, idx = torch.max(rewards_all_actions + v_out, dim=1)