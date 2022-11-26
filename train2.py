02ae9d42396d8211582c62892f0e15fccf1afbf4
Hariganesh Srinivasan <hari.ganesh.friendship@gmail.com>
2022-11-25 20:33:43 -0500

    removed trash

--- train2.py
+++ train2.py
@@ -13,24 +13,44 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--batch_size', default=10000, type=int)
 parser.add_argument('--lr', default=5e-4, type=float)
-parser.add_argument('--nepochs', default=5000, type=int, help='Number of ADI epochs (M)')
-parser.add_argument('--nscrambles', default=30, type=int, help='Number of scrambles of cube (k)')
-parser.add_argument('--nsequences', default=100, type=int, help='Number of sequences of scrambles (l)')
+parser.add_argument('--nepochs',
+                    default=5000,
+                    type=int,
+                    help='Number of ADI epochs (M)')
+parser.add_argument('--nscrambles',
+                    default=30,
+                    type=int,
+                    help='Number of scrambles of cube (k)')
+parser.add_argument('--nsequences',
+                    default=100,
+                    type=int,
+                    help='Number of sequences of scrambles (l)')
 parser.add_argument('--gpu', default='0', type=str)
 parser.add_argument('--wd', default=0, type=int, help="Weight decay")
 parser.add_argument('--momentum', default=0, type=int, help="Momentum")
-parser.add_argument('--tau', default=0.99, type=float, help="Interpolation parameter in soft update")
+parser.add_argument('--tau',
+                    default=0.99,
+                    type=float,
+                    help="Interpolation parameter in soft update")
 parser.add_argument('--device', default="cuda", type=str)
 
-parser.add_argument('--resume_path', default=None, type=str, help="Path of model dict to resume from")
-parser.add_argument('--save_path', default="/home/saicharanb56/RubiksCube-MCTS/results/", type=str, help="Folder in which results are stored")
+parser.add_argument('--resume_path',
+                    default=None,
+                    type=str,
+                    help="Path of model dict to resume from")
+parser.add_argument('--save_path',
+                    default="/home/saicharanb56/RubiksCube-MCTS/results/",
+                    type=str,
+                    help="Folder in which results are stored")
+
 
 def init_weights(m):
     if isinstance(m, nn.Linear):
         torch.nn.init.xavier_uniform(m.weight)
 
+
 def soft_update(local_model, target_model, tau):
-        """Soft update model parameters.
+    """Soft update model parameters.
         θ_target = τ*θ_local + (1 - τ)*θ_target
         Params
         =======
@@ -38,9 +58,11 @@ def soft_update(local_model, target_model, tau):
             target model (PyTorch model): weights will be copied to
             tau (float): interpolation parameter
         """
-        for target_param, local_param in zip(target_model.parameters(),
-                                           local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1-tau)*target_param.data)
+    for target_param, local_param in zip(target_model.parameters(),
+                                         local_model.parameters()):
+        target_param.data.copy_(tau * local_param.data +
+                                (1 - tau) * target_param.data)
+
 
 def generate_scrambled_states(args, cube):
     '''
@@ -57,14 +79,15 @@ def generate_scrambled_states(args, cube):
         # define state, current_state is stored in env instance
         for d in range(args.nscrambles):
             cube.scramble(d)
-            cur_state = cube.get_state() # parent state for this iteration
+            cur_state = cube.get_state()  # parent state for this iteration
             scrambled_states.append(cur_state)
-            weights.append(1/(d + 1))
+            weights.append(1 / (d + 1))
 
         # set cube back to solved state
         cube = Cube.cube_qtm()
 
     return scrambled_states, weights
+
 
 def generate_input_states(cube, scrambled_states):
     '''
@@ -76,11 +99,20 @@ def generate_input_states(cube, scrambled_states):
 
     for i in range(len(scrambled_states)):
         cube.set_state(scrambled_states[i])
-        input_states[i, :] = torch.tensor(cube.representation(), dtype=torch.float32)
+        input_states[i, :] = torch.tensor(cube.representation(),
+                                          dtype=torch.float32)
 
     return input_states
 
-def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_actions=12):
+
+def adi(args,
+        model,
+        model_target,
+        cube,
+        lossfn_prob,
+        lossfn_val,
+        optimizer,
+        n_actions=12):
     '''
     Performs Autodidactic iteration and trains the neural network
     Args:
@@ -98,7 +130,7 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
 
     model = model.to(args.device)
     model_target = model_target.to(args.device)
-    
+
     # initialize weights using Glorot/Xavier initialization
     if args.resume_path:
         resume_state = torch.load(args.resume_path, map_location=args.device)
@@ -123,15 +155,18 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
     for epoch in range(startEpoch, args.nepochs):
         tic = time.time()
 
-        batch_size = args.nscrambles*args.nsequences
+        batch_size = args.nscrambles * args.nsequences
 
         # generate N = K*L scrambled states
         scrambled_states, weights = generate_scrambled_states(args, cube)
         weights = torch.tensor(weights, device=args.device)
         # weights = weights/torch.sum(weights)
-        
-        next_states = torch.empty((batch_size, n_actions, 480), device=args.device)
-        rewards_all_actions = torch.full((batch_size, n_actions, 1), fill_value = -1.0, device=args.device)
+
+        next_states = torch.empty((batch_size, n_actions, 480),
+                                  device=args.device)
+        rewards_all_actions = torch.full((batch_size, n_actions, 1),
+                                         fill_value=-1.0,
+                                         device=args.device)
 
         for l in range(batch_size):
             # set state
@@ -142,9 +177,10 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
             for action in range(n_actions):
                 # perform action
                 cube.turn(action)
-                
+
                 # define next state, reward
-                next_states[l, action, :] = torch.from_numpy(cube.representation()).float()
+                next_states[l, action, :] = torch.from_numpy(
+                    cube.representation()).float()
 
                 if cube.solved():
                     rewards_all_actions[l, action] = 1.0
@@ -155,7 +191,9 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
         print('{0:.10f}'.format(torch.mean(rewards_all_actions).item()))
         # forward pass
         with torch.no_grad():
-            p_out, v_out = model_target(next_states) # next_states shape is (batchsize, n_actions, 480)
+            p_out, v_out = model_target(
+                next_states
+            )  # next_states shape is (batchsize, n_actions, 480)
 
             # print("Probs mean: ", p_out.mean(dim=0))
             print("Val mean: ", v_out.mean(dim=0))
@@ -167,14 +205,14 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
             # p_label = torch.gather(p_out, dim=1, index=idx)
             # p_label = p_label.squeeze(1)
 
-            p_label = p_out[torch.arange(0, batch_size)), idx,:]
+            p_label = p_out[torch.arange(0, batch_size), idx, :]
 
         # training
         input_states = generate_input_states(cube, scrambled_states)
         input_states = input_states.to(args.device)
 
         optimizer.zero_grad()
-        
+
         probs_pred, val_pred = model(input_states)
 
         # calculate sample weighted losses
@@ -190,24 +228,37 @@ def adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer, n_a
         losses_mse.append(loss_val.mean().item())
 
         print('Epoch: [{0}]\t'
-                'CE Loss {1:.8f}\t'
-                'MSE Loss: {2:.8f}  T: {3:.2f}\n'.format(
-                    epoch+1, losses_ce[-1],
-                    losses_mse[-1], time.time() - tic 
-                ))
-
+              'CE Loss {1:.8f}\t'
+              'MSE Loss: {2:.8f}  T: {3:.2f}\n'.format(epoch + 1,
+                                                       losses_ce[-1],
+                                                       losses_mse[-1],
+                                                       time.time() - tic))
 
         soft_update(model, model_target, tau=args.tau)
 
         # save best models
-        saveBestModel(args, losses_ce[-1] + losses_mse[-1], epoch, model, model_target, optimizer, losses_ce, losses_mse)
+        saveBestModel(args, losses_ce[-1] + losses_mse[-1], epoch, model,
+                      model_target, optimizer, losses_ce, losses_mse)
 
         # save this epoch's model and delete previous epoch's model
-        state = {'state_dict': model.state_dict(), 'target_state_dict': model_target.state_dict(),
-                 'optimizer': optimizer.state_dict(), 'ce_losses': losses_ce, 'mse_losses': losses_mse, 'epoch': epoch}
-        torch.save(state, os.path.join(args.save_path, 'checkpoint_' + str(epoch+1) + '.pt'))
-        if os.path.exists(os.path.join(args.save_path, 'checkpoint_' + str(epoch) + '.pt')):
-            os.remove(os.path.join(args.save_path, 'checkpoint_' + str(epoch) + '.pt'))
+        state = {
+            'state_dict': model.state_dict(),
+            'target_state_dict': model_target.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'ce_losses': losses_ce,
+            'mse_losses': losses_mse,
+            'epoch': epoch
+        }
+        torch.save(
+            state,
+            os.path.join(args.save_path,
+                         'checkpoint_' + str(epoch + 1) + '.pt'))
+        if os.path.exists(
+                os.path.join(args.save_path,
+                             'checkpoint_' + str(epoch) + '.pt')):
+            os.remove(
+                os.path.join(args.save_path,
+                             'checkpoint_' + str(epoch) + '.pt'))
 
     return model
 
@@ -221,7 +272,10 @@ if __name__ == "__main__":
     model = NNet()
     model_target = NNet()
 
-    optimizer = optim.RMSprop(model.parameters(), lr=args.lr, weight_decay=args.wd, momentum=args.momentum)
+    optimizer = optim.RMSprop(model.parameters(),
+                              lr=args.lr,
+                              weight_decay=args.wd,
+                              momentum=args.momentum)
     lossfn_val = nn.MSELoss(reduction='none')
     lossfn_prob = nn.CrossEntropyLoss(reduction='none')
 
@@ -229,5 +283,6 @@ if __name__ == "__main__":
     cube = Cube.cube_qtm()
 
     print('ADI started')
-    model = adi(args, model, model_target, cube, lossfn_prob, lossfn_val, optimizer)
+    model = adi(args, model, model_target, cube, lossfn_prob, lossfn_val,
+                optimizer)
     print('ADI done')
